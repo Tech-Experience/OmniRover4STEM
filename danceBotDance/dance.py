d0c39c9bbfc63318d5aa854b551bd7fbfa80f999
Rowin Andruscavage <Rowin.Andruscavage1@T-Mobile.com>
2023-03-22 12:44:17 -0700

    Add dance script

---
+++ danceBotDance/dance.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3 -W
+
+# Command the robots to dance in unison
+
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+#client.connect("mqtt.eclipseprojects.io", 1883, 60)
+client.connect("test.mosquitto.org", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
+
+msgs = [{'topic':"paho/test/multiple", 'payload':"multiple 1"},
+    ("paho/test/multiple", "multiple 2", 0, False)]
+publish.multiple(msgs, hostname="test.mosquitto.org")
