8cc11a4cf8861c27e8eaf9bd8a62dc62e579f335
Rowin Andruscavage <Rowin.Andruscavage1@T-Mobile.com>
2023-03-22 18:07:47 -0700

    Publish the last dance to all rovers

--- danceBotDance/dance.py
+++ danceBotDance/dance.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3 -W
 
 # Command the robots to dance in unison
+# Sends the last command action string to all robots
+
+# How to run and trigger this script:
+#    pipenv install
+#    pipenv shell
+#    python3 dance.py
+#
+# Trigger using an app such as "IoT MQTT Panel"
+# to send any message to the topic RoverAction/stemdance
 
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+import simplejson as json
 
-actions = '{"actions":[{"action":"move_forward","value":1.0},{"action":"move_right","value":1.0},{"action":"move_backward","value":1.0},{"action":"set_speed","value":2.0}]}'
+# Default dance moves
+actions = json.dumps({"actions":[
+    {"action":"move_forward","value":1.0},
+    {"action":"turn_left","value":1.0},
+    {"action":"turn_right","value":1.0},
+    {"action":"set_speed","value":1.0},
+    {"action":"move_backward","value":1.0},
+    {"action":"set_speed","value":2.0}
+]})
 
 def danceRoutine(actions):
     msgs = []
@@ -16,16 +34,16 @@ def danceRoutine(actions):
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("stemdance")
+    client.subscribe("RoverAction/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     global actions
     print(msg.topic+" "+str(msg.payload))
-    danceRoutine(actions)
+    if msg.topic == ('RoverAction/stemdance'):
+        danceRoutine(actions)
+    else:
+        actions = msg.payload
 
 client = mqtt.Client()
 client.on_connect = on_connect
